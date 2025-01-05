a5942b696c6cfe4dd28bb821e139427568c0a69b
Ryuu 48 <auryfieroumarva@gmail.com>
2025-01-05 23:40:39 +0700

    adding security

---
+++ security.js
@@ -0,0 +1,80 @@
+function _0x3e2c() {
+  var _0x2d11fc = [
+    "ctrlKey",
+    "key",
+    "151996EwkfyU",
+    "Event\x20not\x20allowed\x20by\x20RyuuG",
+    "3MjVKtz",
+    "1060084cyZmqH",
+    "419094uamKhr",
+    "312eVfNhJ",
+    "34657oQFurB",
+    "shiftKey",
+    "1570blQJmX",
+    "addEventListener",
+    "224994ZxEsoP",
+    "onload",
+    "2914488ubItXq",
+    "70aeMZMf",
+    "6630IqZayL",
+    "F12",
+    "preventDefault",
+  ];
+  _0x3e2c = function () {
+    return _0x2d11fc;
+  };
+  return _0x3e2c();
+}
+function _0x1860(_0x37f844, _0x5e02e9) {
+  var _0x3e2cc2 = _0x3e2c();
+  return (
+    (_0x1860 = function (_0x186092, _0x198c06) {
+      _0x186092 = _0x186092 - 0x187;
+      var _0x29d137 = _0x3e2cc2[_0x186092];
+      return _0x29d137;
+    }),
+    _0x1860(_0x37f844, _0x5e02e9)
+  );
+}
+var _0x4b29ca = _0x1860;
+(function (_0x505f0f, _0x3e3835) {
+  var _0x36dc82 = _0x1860,
+    _0x4f935c = _0x505f0f();
+  while (!![]) {
+    try {
+      var _0x5ab89f =
+        parseInt(_0x36dc82(0x18d)) / 0x1 +
+        -parseInt(_0x36dc82(0x189)) / 0x2 +
+        (-parseInt(_0x36dc82(0x18b)) / 0x3) *
+          (-parseInt(_0x36dc82(0x18c)) / 0x4) +
+        (-parseInt(_0x36dc82(0x191)) / 0x5) *
+          (parseInt(_0x36dc82(0x197)) / 0x6) +
+        (-parseInt(_0x36dc82(0x18f)) / 0x7) *
+          (parseInt(_0x36dc82(0x18e)) / 0x8) +
+        parseInt(_0x36dc82(0x195)) / 0x9 +
+        (-parseInt(_0x36dc82(0x196)) / 0xa) *
+          (parseInt(_0x36dc82(0x193)) / 0xb);
+      if (_0x5ab89f === _0x3e3835) break;
+      else _0x4f935c["push"](_0x4f935c["shift"]());
+    } catch (_0x443d2f) {
+      _0x4f935c["push"](_0x4f935c["shift"]());
+    }
+  }
+})(_0x3e2c, 0x3cb88);
+function blockRightClickAndInspect() {
+  var _0x75bcd9 = _0x1860;
+  document[_0x75bcd9(0x192)]("contextmenu", (_0xd2f0f9) => {
+    var _0x319910 = _0x75bcd9;
+    _0xd2f0f9[_0x319910(0x199)](), alert(_0x319910(0x18a));
+  }),
+    document[_0x75bcd9(0x192)]("keydown", (_0x1ef5a8) => {
+      var _0x3254e4 = _0x75bcd9;
+      (_0x1ef5a8[_0x3254e4(0x188)] === _0x3254e4(0x198) ||
+        (_0x1ef5a8[_0x3254e4(0x187)] &&
+          _0x1ef5a8[_0x3254e4(0x190)] &&
+          _0x1ef5a8[_0x3254e4(0x188)] === "I")) &&
+        (_0x1ef5a8[_0x3254e4(0x199)](),
+        alert("Event\x20not\x20allowed\x20by\x20RyuuG"));
+    });
+}
+window[_0x4b29ca(0x194)] = blockRightClickAndInspect;
